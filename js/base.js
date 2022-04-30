3e17230fb1ce458f549845ebbf696d635f2ce727
bacon0323 <bacon0323.kn@gmail.com>
2022-04-29 21:58:42 -0700

    changes

--- js/base.js
+++ js/base.js
@@ -1,9 +1,5 @@
-var time = new Date();
-var dd = String(time.getDate()).padStart(2, '0');
-var mm = String(time.getMonth() + 1).padStart(2, '0');
-var yyyy = time.getFullYear();
-time = mm + '/' + dd + '/' + yyyy;
-document.querySelector('#time').innerHTML = time;
+document.lastModified
+document.querySelector('#time').innerHTML = document.lastModified;
 
 var year = new Date(document.lastModified);
 document.querySelector('#year').innerHTML = year.getFullYear();
